2e8ef58b14e4dc4a383b9023e4bad5b3cca6efa5
semperry <ryan.curtis88@gmail.com>
2020-01-13 17:02:05 -0700

    Initial heroku push

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+from flask_heroku import Heroku
 import os
 
 app = Flask(__name__)
 CORS(app)
+heroku = Heroku(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
@@ -13,6 +15,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "ap
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+# push to github and heroku
 class Todo(db.Model):
   __tablename__ = "todos"
   id = db.Column(db.Integer, primary_key=True)
